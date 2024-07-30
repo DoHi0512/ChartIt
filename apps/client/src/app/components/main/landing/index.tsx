3669f1d28bca019e62e421100446018ea5c1a792
dohi0512 <donghuni512@gmail.com>
2024-07-30 22:20:05 +0900

    Feat:: 메인 페이지 랜딩화면

---
+++ apps/client/src/app/components/main/landing/index.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <div className="border-b border-gray-400 py-8">
+      <div className="h-96 w-full rounded-lg border border-black">HI</div>
+    </div>
+  );
+};
+
+export default Landing;
