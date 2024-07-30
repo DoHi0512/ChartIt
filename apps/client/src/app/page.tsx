b976967d6effacbcad9a6a585eea15ad4108727e
dohi0512 <donghuni512@gmail.com>
2024-07-30 22:20:19 +0900

    Feat:: 메인페이지

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@chartit/ui";
+import Landing from "./components/main/landing";
+import GraphList from "./components/main/graph";
 
 export default function Home() {
   return (
     <div>
-      <Button />
+      <Landing />
+      <GraphList />
     </div>
   );
 }
