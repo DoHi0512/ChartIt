831af47b6076a0aaeaff46d0c3196a9e0e0aa169
dohi0512 <donghuni512@gmail.com>
2024-08-05 22:08:23 +0900

    Chore:: 데이터 추가

---
+++ apps/client/src/constants/graph.ts
@@ -0,0 +1,95 @@
+const GRAPH_DATA = {
+  right: [
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+    {
+      name: "막대",
+      image: "bar",
+    },
+  ],
+  left: [],
+};
+
+export default GRAPH_DATA;
