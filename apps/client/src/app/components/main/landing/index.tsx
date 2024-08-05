3cb37921519e44bc58ebf9d55620da2d62d096f0
dohi0512 <donghuni512@gmail.com>
2024-08-05 22:09:13 +0900

    Style:: 디자인 변경

--- apps/client/src/app/components/main/landing/index.tsx
+++ apps/client/src/app/components/main/landing/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@chartit/ui";
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center gap-6 border-b border-gray-400 px-16 py-8">
+    <div className="flex flex-col items-center gap-6 border-gray-400 p-16">
       <span className="text-5xl font-bold">
         쉽고 빠르게 원하는 차트를 그려보세요
       </span>
@@ -9,8 +9,8 @@ const Landing = () => {
         36가지의 차트를 손쉽게 그리고 수정하세요.
       </span>
       <Button
-        sizes="lg"
-        className="bg-primary rounded-md font-medium text-white"
+        sizes="xl"
+        className="rounded-md bg-primary py-3 font-medium text-white hover:opacity-90"
       >
         지금 시작하기
       </Button>
