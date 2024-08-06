a4d848e98796ca6841e66fcb5af17db1a2bf796b
dohi0512 <donghuni512@gmail.com>
2024-08-06 23:30:03 +0900

    Feat:: 로그인 로직

--- apps/client/src/components/common/header/index.tsx
+++ apps/client/src/components/common/header/index.tsx
@@ -1,16 +1,29 @@
-"use client";
-import { getLogin } from "@/service/login/api";
+import useUser from "@/hooks/useUser";
+import Link from "next/link";
 
 export const Header = () => {
+  const { user } = useUser();
+  console.log(user);
   return (
     <div className="fixed z-10 flex h-16 w-full items-center justify-between bg-white px-16 shadow-md">
-      <span className="text-2xl font-semibold">Chart It</span>
-      <span
-        className="cursor-pointer duration-300 hover:opacity-60"
-        onClick={() => getLogin()}
-      >
-        로그인
-      </span>
+      <Link href="/" className="text-2xl font-semibold">
+        Chart It
+      </Link>
+      {user ? (
+        <Link
+          href="/login"
+          className="cursor-pointer duration-300 hover:opacity-60"
+        >
+          {user.email}
+        </Link>
+      ) : (
+        <Link
+          href="/login"
+          className="cursor-pointer duration-300 hover:opacity-60"
+        >
+          로그인
+        </Link>
+      )}
     </div>
   );
 };
--- apps/client/src/service/login/api.ts
+++ apps/client/src/service/login/api.ts
@@ -1,12 +1,11 @@
 import createClient from "@/utils/supabase/client";
-
 const supabase = createClient();
-export const getLogin = async () => {
+export const signIn = async (provider: "google" | "github") => {
   const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "github",
+    provider: provider,
     options: {
-      redirectTo: "/",
+      redirectTo: "http://localhost:3000/auth",
     },
   });
-  return { data, error };
+  console.log(data, error);
 };
