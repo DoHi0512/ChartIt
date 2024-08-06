3ff61d799695eb9d143dced29c6714c66d7a8206
dohi0512 <donghuni512@gmail.com>
2024-08-06 23:30:12 +0900

    Set:: provider 설정

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,16 +1,24 @@
-import { Header } from "./components/common/header";
+"use client";
+import { RecoilRoot } from "recoil";
+import { Header } from "../components/common/header";
 import "./globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="">
-        <Header />
-        <div className="pt-16">{children}</div>
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <html lang="en">
+          <body className="">
+            <Header />
+            <div className="h-full pt-16">{children}</div>
+          </body>
+        </html>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
