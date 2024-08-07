1ba1d2153a48bad8235bcbefc0fb5a6f67a61013
dohi0512 <donghuni512@gmail.com>
2024-08-07 22:06:40 +0900

    Set:: toast container

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { RecoilRoot } from "recoil";
 import { Header } from "../components/common/header";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const queryClient = new QueryClient();
 export default function RootLayout({
   children,
@@ -16,6 +18,7 @@ export default function RootLayout({
           <body className="">
             <Header />
             <div className="h-full pt-16">{children}</div>
+            <ToastContainer />
           </body>
         </html>
       </RecoilRoot>
