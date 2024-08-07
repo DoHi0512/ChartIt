8d4ccf2088df4bfe1e04d91fe893c5c9ed4845d7
dohi0512 <donghuni512@gmail.com>
2024-08-07 22:06:27 +0900

    Add:: recoil persist

--- apps/client/src/store/user.ts
+++ apps/client/src/store/user.ts
@@ -1,8 +1,10 @@
 import { atom, useRecoilState } from "recoil";
-
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
 const userState = atom<any>({
   key: "user",
   default: {},
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const useUserStore = () => useRecoilState(userState);
