81c5473cc68e00949c46fff9dada521c068b0482
dohi0512 <donghuni512@gmail.com>
2024-08-07 21:24:54 +0900

    Fix:: hydration 에러 수정

--- apps/client/src/components/common/header/index.tsx
+++ apps/client/src/components/common/header/index.tsx
@@ -1,21 +1,23 @@
 import useUser from "@/hooks/useUser";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
   const { user } = useUser();
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   console.log(user);
   return (
     <div className="fixed z-10 flex h-16 w-full items-center justify-between bg-white px-16 shadow-md">
       <Link href="/" className="text-2xl font-semibold">
         Chart It
       </Link>
-      {user ? (
-        <Link
-          href="/login"
-          className="cursor-pointer duration-300 hover:opacity-60"
-        >
+      {user && isClient ? (
+        <span className="cursor-pointer duration-300 hover:opacity-60">
           {user.email}
-        </Link>
+        </span>
       ) : (
         <Link
           href="/login"
