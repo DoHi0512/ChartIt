437450df9df852ebbee359a1f380916217ca5060
dohi0512 <donghuni512@gmail.com>
2024-08-08 23:03:46 +0900

    Add:: 그래프 옵션 데이터

--- apps/client/src/constants/graph.ts
+++ apps/client/src/constants/graph.ts
@@ -1,4 +1,4 @@
-const GRAPH_DATA = {
+export const GRAPH_DATA = {
   right: [
     {
       name: "막대",
@@ -91,5 +91,18 @@ const GRAPH_DATA = {
   ],
   left: [],
 };
-
-export default GRAPH_DATA;
+export const GRAPH_TYPE = {
+  bar: {
+    description:
+      "Bar chart which can display multiple data series, stacked or side by side. Also supports both vertical and horizontal layout, with negative values descending below the x axis (or y axis if using horizontal layout).",
+    options: {
+      base: [
+        { name: "data", optional: false, desc: "Chart data" },
+        { name: "data", optional: false, desc: "Chart data" },
+        { name: "data", optional: false, desc: "Chart data" },
+        { name: "data", optional: false, desc: "Chart data" },
+      ],
+      style: [],
+    },
+  },
+};
