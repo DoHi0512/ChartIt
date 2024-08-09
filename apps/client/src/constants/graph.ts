ab3d64a23b7514a66de523f231d41a8746408a81
dohi0512 <donghuni512@gmail.com>
2024-08-09 23:11:48 +0900

    Chore:: 필요없는 코드 삭제

--- apps/client/src/constants/graph.ts
+++ apps/client/src/constants/graph.ts
@@ -91,18 +91,3 @@ export const GRAPH_DATA = {
   ],
   left: [],
 };
-export const GRAPH_TYPE = {
-  bar: {
-    description:
-      "Bar chart which can display multiple data series, stacked or side by side. Also supports both vertical and horizontal layout, with negative values descending below the x axis (or y axis if using horizontal layout).",
-    options: {
-      base: [
-        { name: "data", optional: false, desc: "Chart data" },
-        { name: "data", optional: false, desc: "Chart data" },
-        { name: "data", optional: false, desc: "Chart data" },
-        { name: "data", optional: false, desc: "Chart data" },
-      ],
-      style: [],
-    },
-  },
-};
