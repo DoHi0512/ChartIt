84a2c94e7baf53c8ac18c98dd762b59a5daab201
dohi0512 <donghuni512@gmail.com>
2024-08-09 23:13:29 +0900

    Chore:: 임시 데이터 대신 서버 데이터 사용

--- apps/client/src/app/draw/[type]/page.tsx
+++ apps/client/src/app/draw/[type]/page.tsx
@@ -1,16 +1,18 @@
+"use client";
 import OptionBar from "@/components/draw/optionbar";
-import { GRAPH_TYPE } from "@/constants/graph";
+import { useGraphDetail } from "@/service/graph/queries";
 
 const DrawPage = ({ params }: { params: { type: "bar" } }) => {
   const { type } = params;
+  const { data, isLoading } = useGraphDetail(type);
   return (
     <div className="flex">
       <div className="w-[40rem]">
         <div className="bg-primary px-8 py-6 text-xl text-white">
           {type.toUpperCase()}
         </div>
-        <div className="p-8">{GRAPH_TYPE[type].description}</div>
-        <OptionBar data={GRAPH_TYPE[type].options} />
+        <div className="p-8">{data?.description}</div>
+        <OptionBar data={data?.graph_category} />
       </div>
     </div>
   );
