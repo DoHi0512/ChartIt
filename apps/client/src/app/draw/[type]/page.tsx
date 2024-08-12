0c5b867554a05d43e256d382afeb53ce2295ec95
dohi0512 <donghuni512@gmail.com>
2024-08-12 22:04:28 +0900

    Feat:: 그래프 미리보기

--- apps/client/src/app/draw/[type]/page.tsx
+++ apps/client/src/app/draw/[type]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import GraphSection from "@/components/draw/graph";
 import OptionBar from "@/components/draw/optionbar";
 import { useGraphDetail } from "@/service/graph/queries";
 
@@ -6,14 +7,15 @@ const DrawPage = ({ params }: { params: { type: "bar" } }) => {
   const { type } = params;
   const { data, isLoading } = useGraphDetail(type);
   return (
-    <div className="flex">
-      <div className="w-[40rem]">
+    <div className="flex w-full">
+      <div className="w-[40rem] flex-shrink-0">
         <div className="bg-primary px-8 py-6 text-xl text-white">
           {type.toUpperCase()}
         </div>
         <div className="p-8">{data?.description}</div>
         <OptionBar data={data?.graph_category} />
       </div>
+      <GraphSection />
     </div>
   );
 };
