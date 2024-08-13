83ba30d9b05af8598e213be0626515bfcfbb0624
dohi0512 <donghuni512@gmail.com>
2024-08-13 22:38:47 +0900

    Refactoring:: 테이블 구조 변경

--- apps/client/src/types/graph.ts
+++ apps/client/src/types/graph.ts
@@ -2,18 +2,12 @@ export interface GraphType {
   id: number;
   description: string;
   name: string;
-  graph_category?: GraphCategoryType[];
-}
-
-export interface GraphCategoryType {
-  category: "base" | "style";
-  id?: number;
-  graph_options: GraphOptionType[];
 }
 
 export interface GraphOptionType {
   name: string;
   optional?: boolean;
   defaultValue?: string | number;
+  category: { category: string };
   desc: string;
 }
