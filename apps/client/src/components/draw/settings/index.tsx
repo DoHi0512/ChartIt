e04ab0024eb87d49c1a057f058ecd60cc6a0cfe7
dohi0512 <donghuni512@gmail.com>
2024-08-15 22:16:42 +0900

    Add:: slide 추가

--- apps/client/src/components/draw/settings/index.tsx
+++ apps/client/src/components/draw/settings/index.tsx
@@ -2,9 +2,11 @@ import { GraphSettingType } from "@/types/graph";
 import Choise from "./choise";
 import DataSetting from "./data";
 import Toggle from "./toggle";
+import Slide from "./slide";
 
 export const ComponentMap: { [key: string]: React.FC<GraphSettingType> } = {
   toggle: Toggle,
   data: DataSetting,
   choise: Choise,
+  slide: Slide,
 };
