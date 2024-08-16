ca0078a81c561883bbc0d139f29676e89ceca717
dohi0512 <donghuni512@gmail.com>
2024-08-16 12:11:27 +0900

    Add:: step 추가

--- apps/client/src/components/draw/settings/toggle.tsx
+++ apps/client/src/components/draw/settings/toggle.tsx
@@ -1,17 +1,18 @@
 import { GraphSettingType } from "@/types/graph";
 
 const Toggle = ({ value, setValue }: GraphSettingType) => {
+  console.log("value", value || value == "true");
   return (
     <div className="flex items-center gap-4">
       <div
         onClick={() => setValue(!value)}
-        className={`${value == "true" ? "bg-primary" : "bg-red-400"} relative flex h-5 w-10 cursor-pointer items-center rounded-full`}
+        className={`${value || value == "true" ? "bg-primary" : "bg-red-400"} relative flex h-5 w-10 cursor-pointer items-center rounded-full`}
       >
         <div
-          className={`${value == "true" ? "right-1" : "right-6"} absolute h-3 w-3 rounded-full bg-white duration-150`}
+          className={`${value || value == "true" ? "right-1" : "right-6"} absolute h-3 w-3 rounded-full bg-white duration-150`}
         />
       </div>
-      <span className="font-light">{value}</span>
+      <span className="font-light">{value?.toString()}</span>
     </div>
   );
 };
