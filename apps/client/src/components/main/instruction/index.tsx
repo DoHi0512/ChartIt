468c385d091d44d575d7045687234cb70be35676
dohi0512 <donghuni512@gmail.com>
2024-08-19 23:12:08 +0900

    Add:: instruct 화면

--- apps/client/src/components/main/instruction/index.tsx
+++ apps/client/src/components/main/instruction/index.tsx
@@ -1,5 +1,15 @@
 const Instruction = () => {
-  return <div className="sticky h-64 bg-gray-200"></div>;
+  return (
+    <div className="flex flex-col items-center px-24 py-12">
+      <div className="relative flex items-center justify-center">
+        <img src="/bg.webp" className="rounded-xl border border-gray-300" />
+        <img
+          src="/example.png"
+          className="absolute w-[70%] rounded-lg shadow-2xl"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Instruction;
