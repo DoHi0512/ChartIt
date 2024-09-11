879c32c39fe308e52bde34971989e4f3f3e7fe50
dohi0512 <donghuni512@gmail.com>
2024-09-11 21:50:57 +0900

    Style:: margin 수정

--- apps/client/src/components/draw/graph/index.tsx
+++ apps/client/src/components/draw/graph/index.tsx
@@ -1,20 +1,40 @@
+"use client";
+import { useOptionValueState } from "@/store/options";
 import { ResponsiveBar } from "@nivo/bar";
 
-const GraphSection = ({ option }: { option: any }) => {
-  console.log(option);
+const GraphSection = () => {
+  const option = useOptionValueState();
   return (
     <div className="h-full flex-1">
       <ResponsiveBar
         {...option}
         data={[
-          { id: 1, fanta: 1100 },
+          { id: "250ml", fanta: 1100 },
           { id: "500ml", cola: 2200, cidar: 2000, fanta: 2100 },
           { id: "1000ml", cola: 3200, cidar: 3000, fanta: 3100 },
+          { id: "1250ml", fanta: 1100 },
+          { id: "1500ml", cola: 2200, cidar: 2000, fanta: 2100 },
+          { id: "21000ml", cola: 3200, cidar: 3000, fanta: 3100 },
         ]}
         keys={["cola", "cidar", "fanta"]}
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        margin={{ top: 50, right: 25, bottom: 50, left: 50 }}
         colors={{ scheme: "nivo" }}
-        colorBy="id" // 색상을 keys 요소들에 각각 적용
+        labelSkipHeight={100}
+        labelSkipWidth={100}
+        // legends={[
+        //   {
+        //     dataFrom: "keys",
+        //     anchor: "bottom-right",
+        //     direction: "column",
+        //     itemDirection: "left-to-right",
+        //     justify: false,
+        //     translateX: 120,
+        //     translateY: 0,
+        //     itemsSpacing: 2,
+        //     itemWidth: 100,
+        //     itemHeight: 20,
+        //   },
+        // ]}
       />
     </div>
   );
