16d210bf1fd88dfd15eae561a2a5e1e7ea5fc501
dohi0512 <donghuni512@gmail.com>
2024-09-25 22:32:37 +0900

    Add:: style옵션 추가

--- apps/client/src/components/draw/optionbar/index.tsx
+++ apps/client/src/components/draw/optionbar/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { GraphOptionType } from "@/types/graph";
 import DrawOption from "./option";
 import { ComponentMap } from "../options";
@@ -20,16 +21,24 @@ const OptionBar = ({ data, option, setOption }: OptionBarProps) => {
       </DrawOption>
     );
   });
-  const Style = data.style.map((props, idx) => (
-    <DrawOption key={idx} {...props}>
-      HI
-    </DrawOption>
-  ));
+  const Style = data.style.map((props, idx) => {
+    const Setting = ComponentMap[props.type];
+    return (
+      <DrawOption key={idx} {...props}>
+        <Setting
+          value={option[props.name]}
+          setValue={(v) => setOption({ ...option, [props.name]: v })}
+          {...props}
+        />
+      </DrawOption>
+    );
+  });
   return (
     <div className="w-full">
       <div className="bg-primary px-8 py-3 font-semibold text-white">Base</div>
       {Base}
       <div className="bg-primary px-8 py-3 font-semibold text-white">Style</div>
+      {Style}
     </div>
   );
 };
