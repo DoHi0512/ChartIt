1621db0b096dc6b36ad58e632c7e566ea6221999
dohi0512 <donghuni512@gmail.com>
2024-10-09 16:39:36 +0900

    Style:: 버튼 스타일 변경

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,18 +5,18 @@ interface BtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 const Button = ({ children, sizes = "md", className, href, ...props }: BtnProps) => {
   const SIZE: any = {
-    xs: "text-xs px-2",
-    sm: "text-sm px-3",
-    md: "text-base px-4",
-    lg: "text-lg px-5",
-    xl: "text-xl px-6",
+    xs: "text-xs px-1",
+    sm: "text-sm px-2",
+    md: "text-base px-3",
+    lg: "text-lg px-4",
+    xl: "text-xl px-5",
     full: "w-full text-base px-4",
     fit: "w-fit text-base px-4",
   };
 
   return (
     <button {...props}>
-      <a href={href} className={`${className} justify-center flex gap-4 items-center ${SIZE[sizes]}`}>
+      <a href={href} className={`${className}  justify-center flex gap-4 items-center ${SIZE[sizes]}`}>
         {children}
       </a>
     </button>
